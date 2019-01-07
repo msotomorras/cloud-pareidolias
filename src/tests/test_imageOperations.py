135006beb73519171b7b2fd757739961cd3a53e1
Marta Soto Morras <msotomorras@ideo.com>
2019-01-07 16:21:37 -0600

    changes in sample images

--- src/tests/test_imageOperations.py
+++ src/tests/test_imageOperations.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import numpy as np 
+import cv2
 sys.path.append("../app")
 
 from imageOperations import ImageOperations
@@ -8,7 +9,7 @@ from imageOperations import ImageOperations
 class TestImageOperations:
 
     image_operations = ImageOperations()
-    img = np.zeros([5,5,1], dtype = np.uint8)
+    img = np.zeros([5, 5, 1], dtype = np.uint8)
     box = [1, 1, 3, 3]
     img[box[1]:box[3], box[0]:box[2]] = 255
 
@@ -22,3 +23,21 @@ class TestImageOperations:
         img_expected = np.zeros([450,600], dtype=np.uint8)
         img_resized = self.image_operations.resize_image(self.img)
         assert np.array_equal(img_resized.shape, img_expected.shape)
+    
+    def test_get_total_area_img(self):
+        expected_area = self.img.shape[0]*self.img.shape[1]
+        area = self.image_operations.get_total_area_img(self.img)
+        assert area == expected_area
+
+    def test_fit_generated_image_to_original_image(self):
+        img = self.img
+        generated_image = np.zeros([1, 1, 1], dtype = np.uint8)
+        generated_image.fill(255)
+        margin = 20
+        new_generated_image = np.zeros([img.shape[0], margin*2 + generated_image.shape[1]], dtype = np.uint8)
+        new_generated_image = cv2.cvtColor(new_generated_image, cv2.COLOR_GRAY2BGR)
+
+        result = self.image_operations.fit_generated_image_to_original_image(img, generated_image)
+        assert result.shape == new_generated_image.shape
+    
+
---
+++ src/tests/test_imageProcessing.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import numpy as np 
+import cv2
+sys.path.append("../app")
+
+from imageProcessing import ImageProcessing
+
+class TestImageProcessing:
+
+    image_processing = ImageProcessing()
+    img = np.zeros([5, 5, 3], dtype = np.uint8)
+    box = [1, 1, 3, 3]
+    img[box[1]:box[3], box[0]:box[2]] = 255
+
+    def test_generate_region_of_interest(self):
+        expected_is_there_new_roi = True
+        is_there_new_roi = self.image_processing.generate_region_of_interest(self.img, 'test.jpg')
+        assert  is_there_new_roi == expected_is_there_new_roi
