f47c79a1e253a99a4a921eb7e62fabdce06e941f
Marta Soto Morras <msotomorras@ideo.com>
2019-01-08 10:52:32 -0600

    add github reference to evaluatePix2Pix.py

--- src/app/evaluate_pix2pix.py
+++ src/app/evaluate_pix2pix.py
@@ -1,3 +1,5 @@
+# from https://github.com/zaidalyafeai/zaidalyafeai.github.io/tree/master/pix2pix
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
